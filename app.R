13c6b3171ea6badaa1c5e506a7976be1d31f0ac6
RentoSaijo <rentosaijo0527@gmail.com>
2025-06-25 14:54:12 +0900

    Add users

--- app.R
+++ app.R
@@ -49,6 +49,30 @@ ui <- dashboardPage(
           lead='Your home for viewing NHL statistics available via the nhlscraper R-package.',
           btnName='Learn about nhlscraper',
           href='https://rentosaijo.github.io/nhlscraper/'
+        ),
+        fluidRow(
+          userBox(
+            collapsible=FALSE,
+            title=userDescription(
+              title='Rento Saijo',
+              subtitle='Author of nhlscraper',
+              image='https://cdn.discordapp.com/attachments/873849017702948927/1387302795295588403/RentoSaijo_Picture.jpeg?ex=685cd9e9&is=685b8869&hm=eb315238ccd29f458a69230e6edb793ac6625019e7848e856372ccc51c02cf81&',
+              type=1
+            ),
+            status='olive',
+            'E: rentosaijo0527@gmail.com'
+          ),
+          userBox(
+            collapsible=FALSE,
+            title=userDescription(
+              title='Lars Skytte',
+              subtitle='Contributor of nhlscraper',
+              image='https://cdn.discordapp.com/attachments/873849017702948927/1387308828495708271/Logo.png?ex=685cdf87&is=685b8e07&hm=9121b274a2d33fd1fbf1b3f0bf8235666201a7ed7ad866e2d084e701e2b7bff1&',
+              type=1
+            ),
+            status='olive',
+            'W: hockey-statistics.com'
+          )
         )
       ),
       tabItem(
