9c9cc68bf3d76ee5720b0b06cb7de04c7ab52340
RentoSaijo <rentosaijo0527@gmail.com>
2025-06-25 14:15:11 +0900

    Create initial structure

---
+++ app.R
@@ -0,0 +1,62 @@
+# Load necessary libraries.
+library(tidyverse)
+library(shiny)
+library(bs4Dash)
+library(nhlscraper)
+
+# Setup user interface.
+ui <- dashboardPage(
+  help=NULL,
+  dark=NULL,
+  scrollToTop=TRUE,
+  
+  title='NHL Statistics',
+  header=dashboardHeader(
+    title=dashboardBrand(
+      title='NHL Statistics',
+      image='https://cdn.discordapp.com/attachments/873849017702948927/1387296286625234944/NHLStatistics.png?ex=685cd3d9&is=685b8259&hm=67725d29c80f96b56efc16773774e9668564d37a1fe8b14bef0626ed57859e8d&'
+    )
+  ),
+  sidebar=dashboardSidebar(
+    sidebarMenu(
+      id='sidebarMenuid',
+      menuItem(
+        'Home',
+        tabName='home',
+        icon=ionicon('home')
+      ),
+      menuItem(
+        'Player',
+        tabName='player',
+        icon=ionicon('person')
+      ),
+      menuItem(
+        'Team',
+        tabName='team',
+        icon=ionicon('people')
+      )
+    )
+  ),
+  controlbar=dashboardControlbar(),
+  footer=dashboardFooter(),
+  body=dashboardBody(
+    tabItems(
+      tabItem(
+        tabName='home'
+      ),
+      tabItem(
+        tabName='player'
+      ),
+      tabItem(
+        tabName='people'
+      )
+    )
+  )
+)
+
+# Setup server.
+server <- function(input, output) {
+}
+
+# Start shiny application.
+shinyApp(ui, server)
